ad4b3bc29f12798613aef31a76db2d2b69853931
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-07-03 10:54:27 -0400

    Counter - Manejo de estados - useState

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,21 @@
+import { CartCounter } from "@/app/shopping-cart/components";
+import { Metadata } from "next/types";
+
+
+export const metadata: Metadata = {
+ title: 'Counter Page',
+ description: 'Un simple contador',
+};
 
 export default function CounterPage() {
+
   return (
-    <div>
-      <h1>Hello Page Counter</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      
+      <span>Productos en el carrito de compras</span>
+      
+      <CartCounter value={20} />
+
     </div>
   );
 }
