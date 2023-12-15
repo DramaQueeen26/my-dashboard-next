1fbc2d8bc41200ab9bb181d292f7456c0150e20e
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-12-14 20:38:03 -0400

    Generación dinámica - SSR

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,21 +1,19 @@
-import { CartCounter } from "@/app/shopping-cart/components";
-import { Metadata } from "next/types";
+import { CartCounter } from "@/shopping-cart";
 
 
-export const metadata: Metadata = {
- title: 'Counter Page',
+export const metadata = {
+ title: 'Shopping Cart',
  description: 'Un simple contador',
 };
+
 
 export default function CounterPage() {
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CartCounter value={ 20 } />
       
-      <span>Productos en el carrito de compras</span>
-      
-      <CartCounter value={20} />
-
     </div>
   );
 }
